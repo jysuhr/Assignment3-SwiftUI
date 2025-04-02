c504b692c3ce5eb157a3450fa132ef7153c12e15
jysuhr <jun0.suhr@gmail.com>
2025-04-02 22:14:26 +0900

    [Setup] 개발환경 세팅

---
+++ Assignment3_SwiftUI/Assignment3_SwiftUI/Assignment3_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment3_SwiftUIApp.swift
+//  Assignment3_SwiftUI
+//
+//  Created by 서준영 on 4/2/25.
+//
+
+import SwiftUI
+
+@main
+struct Assignment3_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assignment3_SwiftUI/Assignment3_SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Assignment3_SwiftUI
+//
+//  Created by 서준영 on 4/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
